365fdcd4cf26891ad716c6e1391e890e87fb9c7f
Alan Ott <alan.ott@ecole-hexagone.com>
2025-04-08 08:30:34 +0000

    app

---
+++ apps/myapp.py
@@ -0,0 +1,11 @@
+# /home/apps/myapp.py
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def welcome():
+    return 'Welcome to flask application'
+@app.route('/test')
+def test():
+    return 'This is test route of myapp flask application'
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
